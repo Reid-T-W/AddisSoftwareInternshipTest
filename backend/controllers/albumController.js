b5ac977dee00009c5263585e8472e8a7bfaf4a2e
Reid-T-W <rediettadesse100@gmail.com>
2024-01-31 21:48:24 +0300

    album controller added

---
+++ backend/controllers/albumController.js
@@ -0,0 +1,21 @@
+const Song = require('../models/songModel')
+const mongoose = require('mongoose')
+
+// get all albums
+const getAlbums = async(req, res) => {
+    
+    const albumsWithSongCount = await Song.aggregate([
+        {
+          $group: {
+            _id: "$album",
+            songCount: { $sum: 1 },
+          },
+        },
+      ]);
+
+    return res.status(200).json({"albumsWithSongCount": albumsWithSongCount})
+}
+
+module.exports = {
+    getAlbums
+}
