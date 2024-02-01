9f57af5a953b4799813352a41d1f3e0cdcfb3981
Reid-T-W <rediettadesse100@gmail.com>
2024-02-01 11:53:07 +0300

    types added for album, artist and genere

--- music-app/src/models/models.ts
+++ music-app/src/models/models.ts
@@ -5,3 +5,24 @@ export interface SongType {
     artist: string;
     genere: string;
 }
+
+export interface AlbumType {
+    id: number;
+    albumName: string;
+    artistName: string;
+    noOfSongs: number;
+}
+
+export interface ArtistType {
+    id: number;
+    artistName: string;
+    noOfSongs: number;
+    noOfAlbums: number;
+}
+
+export interface GenereType {
+    id: number;
+    genereName: string;
+    noOfAlbums: number;
+    noOfSongs: number;
+}
