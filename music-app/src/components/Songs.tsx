53f65f4d4d09343eff94fa74362d330fb1ad8354
Reid-T-W <rediettadesse100@gmail.com>
2024-02-01 11:47:05 +0300

    Songs component added

--- music-app/src/components/Songs.tsx
+++ music-app/src/components/Songs.tsx
@@ -2,37 +2,73 @@ import React, { useEffect, useState } from 'react'
 import { SongType } from '../models/models'
 import { Song } from './'
 import { SongsStyled } from './styled/SongsStyled'
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../state/store"
+import { addSong, removeSong, updateSong } from '../state/songs/songsSlice';
 
-interface SongsStateType {
-    songs: SongType[];
-    setSongs: React.Dispatch<React.SetStateAction<SongType[]>>;
-}
+// interface SongsStateType {
+//     songs: SongType[];
+//     setSongs: React.Dispatch<React.SetStateAction<SongType[]>>;
+// }
 
 const Songs = () => {
-  const [songs, setSongs] = useState<SongType[]>([])
+    const songs = useSelector((state: RootState) => state.songs.songs)
+    const dispatch = useDispatch();
 
-  let songsList:SongType[] = [
-    {id: 1, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
-    {id: 2, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
-    {id: 3, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
-    {id: 4, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'}
-  ]
+//   const [songs, setSongs] = useState<SongType[]>([])
 
-  useEffect(()=>{
-    songsList.forEach((item: SongType) => {
-        // console.log("song: ", song)
-        // setSongs([...songs, item])
-        setSongs(prevSongs => [...prevSongs, item]);
-      })
-  },[])
+//   let songsList:SongType[] = [
+//     {id: 1, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
+//     {id: 2, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
+//     {id: 3, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'},
+//     {id: 4, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'}
+//   ]
+
+//   useEffect(()=>{
+//     songsList.forEach((item: SongType) => {
+//         // console.log("song: ", song)
+//         // setSongs([...songs, item])
+//         setSongs(prevSongs => [...prevSongs, item]);
+//       })
+//   },[])
 
   
-  console.log("songs", songs)
+//   console.log("songs", songs)
+  useEffect(()=>{
+    dispatch({type: 'GET_SONGS_REQUESTED'})
+  }, [dispatch])
 
   return (
     <>
             <SongsStyled>
               Songs
+
+              <div className="flex-table">
+                <div className="flex-table-header">
+                  <div className="flex-table-data heading-text">Name</div>
+                  <div className="flex-table-data heading-text">Album</div>
+                  <div className="flex-table-data heading-text">Artist</div>
+                  <div className="flex-table-data heading-text">Genere</div>
+                </div>
+
+                {songs.map((song: SongType) => (
+                <Song key={song.id}
+                  songId={song.id}
+                  songName={song.songName} 
+                  songAlbum = {song.album}
+                  songArtist = {song.artist}
+                  songGenere = {song.genere}
+                />
+              ))}
+
+
+            </div>
+
+
+
+              {/* <button onClick={() => dispatch(addSong({id: 1, songName: 'Song1', album: 'Album1', artist: 'Artist1', genere: 'Genere1'}))}>Add Song</button>
+              <button onClick={() => dispatch(removeSong(1))}>Remove Song</button>
+              <button onClick={() => dispatch(updateSong([1, {id: 1, songName: 'UpdatedSong', album: 'UpdatedAlbum', artist: 'UpdatedArtist', genere: 'UpdatedGenere'}]))}>Update Song</button> */}
             </SongsStyled>
             {/* {
                 songs?.map((song: SongType) => (
