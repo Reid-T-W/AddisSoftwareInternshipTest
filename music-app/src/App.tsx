fac84cd5fd64f6a4fcbaa4af43e51cbac35da167
Reid-T-W <rediettadesse100@gmail.com>
2024-02-03 13:12:14 +0300

    Components to be rendred in Content are passed as props

--- music-app/src/App.tsx
+++ music-app/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Body, Navbar, Sidebar, Footer } from './components'
+import { Body, 
+         Navbar, 
+         Sidebar, 
+         Footer, 
+         Songs, 
+         Albums, 
+         Artists, 
+         Geners } from './components'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -9,7 +16,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Body/>} />
+          <Route path="/" element={<Body Component={Songs}/>} />
+          <Route path="/albums" element={<Body Component={Albums}/>} />
+          <Route path="/artists" element={<Body Component={Artists}/>} />
+          <Route path="/generes" element={<Body Component={Geners}/>} />
         </Routes>
         </BrowserRouter>
     </div>
