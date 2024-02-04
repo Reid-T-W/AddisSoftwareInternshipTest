dcac6bb85568f21726534328241453461bf7b3ed
Reid-T-W <rediettadesse100@gmail.com>
2024-02-04 16:06:58 +0300

    input form validation added

--- music-app/src/components/Songs/Songs.tsx
+++ music-app/src/components/Songs/Songs.tsx
@@ -8,6 +8,16 @@ import { Button, Flex, Box } from 'rebass'
 import { InputStyled } from '../styled/InputStyled';
 import { TitleStyled } from '../styled/TitleStyled';
 import { TableStyled } from '../styled/TableStyled';
+import { addSongFailed } from '../../state/songs/songsSlice';
+import { toast } from 'react-toastify';
+import { error } from 'console';
+
+interface validationErrorsType {
+  title?: string,
+  album?: string,
+  artist?: string,
+  genre?: string
+}
 
 const Songs = () => {
   const [localSongName, setLocalsongname] = useState("")
@@ -17,6 +27,9 @@ const Songs = () => {
 
   const songs = useSelector((state: RootState) => state.songs.songs)
   const dispatch = useDispatch();
+
+  const [errors, setErrors] = useState<validationErrorsType>({})
+  let validationErrors:validationErrorsType = {}
 
   useEffect(()=>{
     dispatch({type: 'GET_SONGS_REQUESTED'})
@@ -32,16 +45,58 @@ const Songs = () => {
     }
   };
 
+  const validateForm = () => {
+    if (!localSongName.trim()) {
+      validationErrors.title = "title is required"
+    }    
+    if (!localSongAlbum.trim()) {
+      validationErrors.album = "album is required"
+    }    
+    if (!localSongArtist.trim()) {
+      validationErrors.artist = "artist is required"
+    }
+    if (!localSongGenere.trim()) {
+      validationErrors.genre = "genre is required"
+    }
+    setErrors(validationErrors);
+  }
+
+  const addSong = () => {
+     // Check if the form is valid
+     // This function populates the validationErrors
+     // dict incase errors are found
+     validateForm()
+     if (Object.keys(validationErrors).length === 0) {
+      // Valid form
+      dispatch({
+        type: 'ADD_SONG_REQUESTED',
+        payload: {
+          title: localSongName,
+          album: localSongAlbum,
+          artist: localSongArtist,
+          genere: localSongGenere}
+        });
+        // Set the input values to empty
+        setLocalsongname('')
+        setLocalsongalbum('')
+        setLocalsongartist('')
+        setLocalsonggenere('')
+        scrollToTop()
+        // Set errors to empty
+        setErrors({})
+      } 
+  }
+
   return (
       <Flex flexDirection={'column'} width={'100%'}>
             <SongsStyled>
               
-              {/* title */}
+              {/* Title */}
               <TitleStyled>
                 Songs
               </TitleStyled>
               
-              {/* list of songs */}
+              {/* List of songs */}
               <TableStyled ref={tableRef}>
 
                 {songs.map((song: SongType) => (
@@ -55,37 +110,37 @@ const Songs = () => {
                 ))}
               </TableStyled>
 
-              {/* add new song */}
+              {/* Add new song */}
               <Flex marginTop={'15px'}>
 
-                {/* input form*/}
+                {/* Input form*/}
                 <Box>
                   <form>
-                    <InputStyled name='songName' type='text' placeholder='Name' value={localSongName} onChange={ (e) => {setLocalsongname(e.target.value)}}/>
-                    <InputStyled name='album' type='text' placeholder='Album' value={localSongAlbum} onChange={(e) => {setLocalsongalbum(e.target.value)}} />
-                    <InputStyled name='artist' type='text' placeholder='Artist' value={localSongArtist} onChange={(e) => {setLocalsongartist(e.target.value)}}/>
-                    <InputStyled name='genere' type='text' placeholder='Genere' value={localSongGenere} onChange={(e) => {setLocalsonggenere(e.target.value)}}/>
+                    <Flex flexDirection={"row"}>
+                      <Flex flexDirection={"column"} color={"red"}>
+                        <InputStyled name='songName' type='text' placeholder='Title' value={localSongName} onChange={ (e) => {setLocalsongname(e.target.value)}}/>
+                        {errors.title && <span>{errors.title}</span>}
+                      </Flex>
+                      <Flex flexDirection={"column"} color={"red"}>
+                        <InputStyled name='album' type='text' placeholder='Album' value={localSongAlbum} onChange={(e) => {setLocalsongalbum(e.target.value)}} />
+                        {errors.album && <span>{errors.album}</span>}
+                      </Flex>
+                      <Flex flexDirection={"column"} color={"red"}>
+                        <InputStyled name='artist' type='text' placeholder='Artist' value={localSongArtist} onChange={(e) => {setLocalsongartist(e.target.value)}}/>
+                        {errors.artist && <span>{errors.artist}</span>}
+                      </Flex>
+                      <Flex flexDirection={"column"} color={"red"}>
+                        <InputStyled name='genere' type='text' placeholder='Genere' value={localSongGenere} onChange={(e) => {setLocalsonggenere(e.target.value)}}/>
+                        {errors.genre && <span>{errors.genre}</span>}
+                      </Flex>
+                    </Flex>
                   </form>
                 </Box>
                 
-                {/* add button */}
+                {/* Add button */}
                 <Box marginLeft={'10px'}>
                   <Button css={{ ":hover": {opacity:'0.5'}}} color={'white'} backgroundColor={"orange"} 
-                    onClick={() => {
-                      dispatch({
-                      type: 'ADD_SONG_REQUESTED',
-                      payload: {
-                        title: localSongName,
-                        album: localSongAlbum,
-                        artist: localSongArtist,
-                        genere: localSongGenere}
-                      });
-                      setLocalsongname('')
-                      setLocalsongalbum('')
-                      setLocalsongartist('')
-                      setLocalsonggenere('')
-                      scrollToTop()
-                  }}>Add Song</Button>
+                    onClick={() => addSong()}>Add Song</Button>
                 </Box>
               </Flex>
             </SongsStyled>
